d601b08579cf3e31c461dc7fecc8a50f87da12af
Ariatna Berganza <135402133+ria14656@users.noreply.github.com>
2023-09-16 19:36:53 -0600

    Add files via upload

---
+++ ejercicios 3/ejercicio1python-.py
@@ -0,0 +1,17 @@
+def mostrar_mensaje(mensaje):
+    print("*****************")
+    print("mensaje)
+    print("************")
+
+    carga_suma()
+    valor1=int(input("ingrese el primer valor:"))
+    valor2=int(input("ingrese el segundo valor:"))
+    suma=valor1+valor2
+    print("la suma de los dos valores es suma")
+
+    #programa principal
+    mostrar_mensaje("la suma de dos valores ingresados por teclado es.")
+    cargar_suma()
+    mostrar_mensaje()
+    
+    
